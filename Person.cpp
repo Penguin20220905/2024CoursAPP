d0e9be96d99986363cb710d2486f33b262ccbcd7
ban Xit <k.zx20404@gmail.com>
2024-11-25 22:31:43 +0800

    加入專案檔案。

---
+++ Person.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include "Person.h"
+#include<string>
+using namespace std;
+
+
+
+Person::Person(const string& id,const string& lastName,const string& firstName,const string& gender,const string& birthDate)
+{
+	this->id = id;
+	this->lastName = lastName;
+	this->firstName = firstName;
+	this->gender = gender;
+	this->birthDate = birthDate;
+}
+
+Person::Person()
+{
+}
+
+string Person::getId() const
+{
+	return id;
+}
+
+string Person::getLastName() const
+{
+	return lastName;
+}
+
+string Person::getFirstName() const
+{
+	return firstName;
+}
+
+string Person::getGender() const
+{
+	return gender;
+}
+
+string Person::getBirthDate() const
+{
+	return birthDate;
+}
+
+void Person::setId(const string& id)
+{
+	this->id = id;
+}
+
+void Person::setLastName(const string& lastName)
+{
+	this->lastName = lastName;
+}
+
+void Person::setFirstName(const string& firstName)
+{
+	this->firstName = firstName;
+}
+
+void Person::setGender(const string& gender)
+{
+	this->gender = gender;
+}
+
+void Person::setBirthDate(const string& birthDate)
+{
+	this->birthDate = birthDate;
+}
+
+void Person::display() const
+{
+	cout << "ID:" << id << endl;
+	cout << "姓:" << lastName << endl;
+	cout << "名:" << firstName << endl;
+	cout << "性別:" << gender << endl;
+	cout << "生日:" << birthDate << endl;
+}

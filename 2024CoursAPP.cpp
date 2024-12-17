afeae6bee4e2c2a34398df6dcb3c3aacce1f611e
Penguin20220905 <Penguin20220905>
2024-12-17 11:49:59 +0800

    新增刪除上一步 暫存

--- 2024CoursAPP.cpp
+++ 2024CoursAPP.cpp
@@ -71,36 +71,47 @@ void initializeData()
 void displayMenu() {
 	int choice;
 	do {
+		system("cls");
 		cout << "==================" << endl;
 		cout << "1. 列出學生資料" << endl;
 		cout << "2. 列出課程資料" << endl;
 		cout << "3. 列出教師資料" << endl;
 		cout << "4. 列出選課紀錄" << endl;
+		cout << "5. 查詢學生資料" << endl;
 		cout << "0. 退出" << endl;
 		cin >> choice;
 		switch (choice) {
 		case 1:
 			cout << "列出學生資料" << endl;
 			listStudents();
+			cout << "按任意鍵繼續..." << endl;
+			system("pause");
 			break;
 		case 2:
 			cout << "列出課程資料" << endl;
-			for (auto course : courses) {
-				course.display();
-			}
+			listCourses();
+			cout << "按任意鍵繼續..." << endl;
+			system("pause");
 			break;
 		case 3:
 			cout << "列出教師資料" << endl;
-			for (auto teacher : teachers) {
-				teacher.display();
-			}
+			listTeachers();
+			cout << "按任意鍵繼續..." << endl;
+			system("pause");
 			break;
 		case 4:
 			cout << "列出選課資料" << endl;
-			for (auto record : records) {
-				record.display();
-			}
+			listRecords();
+			cout << "按任意鍵繼續..." << endl;
+			system("pause");
 			break;
+		/*case 5:
+			cout << "查詢學生資料" << endl;
+			for(auto student:students)
+				if (student.getStudentI) {
+
+				}
+			break;*/
 		case 0:
 			cout << "退出" << endl;
 			break;
@@ -109,6 +120,39 @@ void displayMenu() {
 			break;
 		}
 	} while (choice != 0);
+}
+
+void listRecords()
+{
+	cout << "選課紀錄總共有" << courses.size() << "筆" << endl;
+	cout << "----------------" << endl;
+	for (auto record : records) {
+		record.display();
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void listTeachers()
+{
+	cout << "教師資料總共有" << courses.size() << "筆" << endl;
+	cout << "----------------" << endl;
+	for (auto teacher : teachers) {
+		teacher.display();
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void listCourses()
+{
+	cout << "列出課程資料" << courses.size() << "筆" << endl;
+	cout << "----------------" << endl;
+	for (auto course : courses) {
+		course.display();
+		cout << endl;
+	}
+	cout << endl;
 }
 
 void listStudents()
--- 2024CoursAPP.h
+++ 2024CoursAPP.h
@@ -1,6 +1,7 @@
 #pragma once
-
 void initializeData();
 void displayMenu();
-
+void listRecords();
+void listTeachers();
+void listCourses();
 void listStudents();
